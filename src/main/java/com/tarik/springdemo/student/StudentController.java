c38b929d35456ce2ab49e4c531b0c27a7f84643b
Tarik <trizvic1@etf.unsa.ba>
2025-05-28 00:39:03 +0200

    building the first sample API

--- src/main/java/com/tarik/springdemo/student/StudentController.java
+++ src/main/java/com/tarik/springdemo/student/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/student")
+@RequestMapping("/api/v1/students")
 public class StudentController {
     @GetMapping
     public List<String> findAllStudents(){
